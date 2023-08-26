b8f99298513384f5179ec650741d4cdba51e062b
Richz <126610839+Richardxxbox380@users.noreply.github.com>
2023-08-25 21:58:10 -0300

    Add files via upload

---
+++ exemplojanelas/Exemplojanelas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.exemplojanelas;
+
+/**
+ *
+ * @author richard.msbernardo
+ */
+public class Exemplojanelas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        TelaPrincipal1 novaTela = new TelaPrincipal1();
+        novaTela.setVisible(true);
+    }
+}
