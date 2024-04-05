d2323b3c8364a1e3d9c2ca6fe1e8b72d490ed903
Richard Miranda São Bernardo <126610839+Rich-Miranda@users.noreply.github.com>
2024-04-05 16:31:34 -0300

    Add files via upload

---
+++ Anim/aa.java
@@ -0,0 +1,16 @@
+package Anim;
+
+public class aa {
+    public static void main(String[] args) {
+        Rato mouse = new Rato(" lonzo " , " kwid " , 1) ;
+
+        System.out.println("aPRESENTAÇÃO");
+    
+
+        System.out.println("Nome : " + mouse.getNome() + " Raca: " + mouse.getRaca() + " Idade : " + mouse.getRaca());
+       mouse.Mostra();
+        //mouse.Habita(); mouse.Familia(); mouse.alimentacao();
+        
+    }
+   
+}
