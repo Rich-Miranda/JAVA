4dee2bc244761737f7b137e85d8fa37172948284
Richard Miranda São Bernardo <126610839+Rich-Miranda@users.noreply.github.com>
2024-05-26 14:06:32 -0300

    Update UsarArvore.java

--- Arvore 2/src/Cliente/UsarArvore.java
+++ Arvore 2/src/Cliente/UsarArvore.java
@@ -2,8 +2,6 @@ package Cliente;
 
 import Arvore.Arvore;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class UsarArvore {
     public static void main(String[] args) {
         Arvore tes = new Arvore();
@@ -19,23 +17,18 @@ public class UsarArvore {
         tes.inserir(1);
         tes.ordem(tes.getRaiz());
         System.out.println();
-        tes.Pos_ordem(tes.getRaiz());
-        System.out.println();
         tes.Preordem(tes.getRaiz());
         System.out.println();
+        tes.Pos_ordem(tes.getRaiz());
         System.out.println();
-        //int n = 24;
+        System.out.println();
+
 
         System.out.println("ele retornou : "+tes.menorValor(tes.getRaiz()));
         System.out.println("ele retornou : "+tes.maiorValor(tes.getRaiz()));
 
 
-//
-//        if(tes.pesquisa(n,tes.getRaiz())){ //verificao na arvore, se existe o numero de pesquisa
-//            System.out.println("Achou valor de n: "+ n);
-//        }else{
-//            System.out.println("Nao achou valor n:" + n);
-//        }
+
 
 
     }
